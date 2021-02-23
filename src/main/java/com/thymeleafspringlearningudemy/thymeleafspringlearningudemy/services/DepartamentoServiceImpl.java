d82a5cc68a8886bdc3c8fef346dfadee9f9f717f
Jaime <jaimelay1997@gmail.com>
2021-02-23 15:02:42 -0300

    feat: add actions from controllers to views from departamento

--- src/main/java/com/thymeleafspringlearningudemy/thymeleafspringlearningudemy/services/DepartamentoServiceImpl.java
+++ src/main/java/com/thymeleafspringlearningudemy/thymeleafspringlearningudemy/services/DepartamentoServiceImpl.java
@@ -44,4 +44,13 @@ public class DepartamentoServiceImpl implements DepartamentoService {
     public List<Departamento> buscarTodos() {
         return dao.findAll();
     }
+
+    @Override
+    public boolean departamentoTemCargos(Long id) {
+        if (buscarPorId(id).getCargos().isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
 }
