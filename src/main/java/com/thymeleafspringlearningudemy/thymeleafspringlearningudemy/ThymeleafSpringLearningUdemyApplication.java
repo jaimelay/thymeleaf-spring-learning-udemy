64f7ca1b87e1c71ba52c7ee394a85a0dec249f08
Jaime <jaimelay1997@gmail.com>
2021-02-22 22:26:42 -0300

    feat: initializing spring boot and add html, css

---
+++ src/main/java/com/thymeleafspringlearningudemy/thymeleafspringlearningudemy/ThymeleafSpringLearningUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.thymeleafspringlearningudemy.thymeleafspringlearningudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSpringLearningUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSpringLearningUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thymeleafspringlearningudemy/thymeleafspringlearningudemy/ThymeleafSpringLearningUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleafspringlearningudemy.thymeleafspringlearningudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSpringLearningUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
